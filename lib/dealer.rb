aa7eb2eeab4f62191fbb2ed5e7dffcb19a9e8e14
sashadev-sky <sboginsky17@gmail.com>
2018-08-06 03:20:32 -0400

    Update dealer specs

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -10,10 +10,6 @@ class Dealer < Player
     @bets = {}
   end
 
-  def place_bet(dealer, amt)
-    raise "Dealer doesn't bet" if dealer.is_a?(Dealer)
-  end
-
   def play_hand(deck)
     hit(deck) until points >= 17
   end
--- spec/dealer_spec.rb
+++ spec/dealer_spec.rb
@@ -3,12 +3,6 @@ require 'dealer'
 describe Dealer do
   subject(:dealer) { Dealer.new }
 
-  it "does not place bets" do
-    expect do
-      dealer.place_bet(dealer, 100)
-    end.to raise_error("Dealer doesn't bet")
-  end
-
   describe "#play_hand" do
     let(:dealer_hand) { double("hand") }
     let(:deck) { double("deck") }
