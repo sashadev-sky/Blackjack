259ca50989720868aee071ad9bbaf690cc89f9aa
sashadev-sky <sboginsky17@gmail.com>
2018-08-08 05:46:37 -0400

    Remove reliance on Card class with doubles

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,33 +1,43 @@
 require 'rspec'
 require 'hand'
-require 'card'
 
 describe Hand do
-  describe "#points" do
-    it "adds up normal cards" do
-      hand = Hand.new([
-          Card.new(:spades, :deuce),
-          Card.new(:spades, :four)
-        ])
+  let(:cards) do
+      [ double("card", :suit => :spades, :value => :deuce, :revealed => true, :blackjack_value => 2),
+        double("card", :suit => :spades, :value => :four, :revealed => true, :blackjack_value => 4)]
+  end
 
+  subject(:hand) { Hand.new(cards) }
+
+  describe '#initialize' do
+    it 'accepts cards correctly' do
+      expect(hand.cards).to match_array(cards)
+    end
+  end
+
+  describe "#points" do
+    let(:high_ace_cards) do
+      [ double("card", :suit => :spades, :value => :ten, :revealed => true, :blackjack_value => 10),
+        double("card", :suit => :spades, :value => :ace, :revealed => true, :blackjack_value => 11)]
+    end
+
+    let(:low_ace_cards) do
+      [ double("card", :suit => :spades, :value => :ace, :revealed => true, :blackjack_value => 11),
+        double("card", :suit => :spades, :value => :six, :revealed => true, :blackjack_value => 6),
+        double("card", :suit => :hearts, :value => :ace, :revealed => true, :blackjack_value => 1)]
+    end
+
+    it "adds up normal cards" do
       expect(hand.points).to eq(6)
     end
 
     it "counts an ace as 11 if it can" do
-      hand = Hand.new([
-          Card.new(:spades, :ten),
-          Card.new(:spades, :ace)
-        ])
-
+      hand = Hand.new(high_ace_cards)
       expect(hand.points).to eq(21)
     end
 
     it "counts some aces as 1 and others as 11" do
-      hand = Hand.new([
-          Card.new(:spades, :ace),
-          Card.new(:spades, :six),
-          Card.new(:hearts, :ace)
-        ])
+      hand = Hand.new(low_ace_cards)
 
       expect(hand.points).to eq(18)
     end
@@ -48,54 +58,39 @@ describe Hand do
 
   describe "#beats?" do
     it "returns true if other hand has fewer points" do
-      hand1 = Hand.new([
-          Card.new(:spades, :ace),
-          Card.new(:spades, :ten)
-        ])
       hand2 = Hand.new([
-          Card.new(:hearts, :ace),
-          Card.new(:hearts, :nine)
+        double("card", :suit => :spades, :value => :deuce, :revealed => true, :blackjack_value => 2),
+        double("card", :suit => :spades, :value => :three, :revealed => true, :blackjack_value => 3)
         ])
 
-      expect(hand1.beats?(hand2)).to be(true)
-      expect(hand2.beats?(hand1)).to be(false)
+      expect(hand.beats?(hand2)).to be(true)
+      expect(hand2.beats?(hand)).to be(false)
     end
 
     it "returns false if hands have equal points" do
-      hand1 = Hand.new([
-          Card.new(:spades, :ace),
-          Card.new(:spades, :ten)
-        ])
-      hand2 = Hand.new([
-          Card.new(:hearts, :ace),
-          Card.new(:hearts, :ten)
-        ])
+      hand2= Hand.new([
+        double("card", :suit => :hearts, :value => :deuce, :revealed => true, :blackjack_value => 2),
+        double("card", :suit => :hearts, :value => :four, :revealed => true, :blackjack_value => 4)
+      ])
 
-      expect(hand1.beats?(hand2)).to be(false)
-      expect(hand2.beats?(hand1)).to be(false)
+      expect(hand.beats?(hand2)).to be(false)
+      expect(hand2.beats?(hand)).to be(false)
     end
 
     it "returns false if busted" do
-      hand1 = Hand.new([
-          Card.new(:spades, :ten),
-          Card.new(:hearts, :ten),
-          Card.new(:clubs, :ten)
-        ])
-      hand2 = Hand.new([
-          Card.new(:hearts, :deuce),
-          Card.new(:hearts, :three)
-        ])
+      hand2= Hand.new([
+        double("card", :suit => :spades, :value => :ten, :revealed => true, :blackjack_value => 10),
+        double("card", :suit => :hearts, :value => :ten, :revealed => true, :blackjack_value => 10),
+        double("card", :suit => :clubs, :value => :ten, :revealed => true, :blackjack_value => 10)
+      ])
 
-      expect(hand1.beats?(hand2)).to be(false)
-      expect(hand2.beats?(hand1)).to be(true)
+      expect(hand2.beats?(hand)).to be(false)
+      expect(hand.beats?(hand2)).to be(true)
     end
   end
 
   describe "#return_cards" do
     let(:deck) { double("deck") }
-    let(:hand) do
-      Hand.new([Card.new(:spades, :deuce), Card.new(:spades, :three)])
-    end
 
     it "returns cards to deck" do
       expect(deck).to receive(:return) do |cards|
